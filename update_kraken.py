7edbf63dae7b6f4c746c764e731d09aa5d938f2a
Skyrossm <skyrossm@gmail.com>
2020-02-05 20:43:54 -0800

    update counters

--- update_kraken.py
+++ update_kraken.py
@@ -90,12 +90,13 @@ def fetch_names():
     #backup for streamers with stupid titles
     streamerList = ['dasMEHDI', 'koil']
 
-    names = [x['channel']['display_name'] for x in data['streams'] if (any(s in x['channel'].get('status', '').lower() for s in wordList) or (x['channel'].get('display_name', '').lower() for s in streamerList)) and x['broadcast_platform']=='live']
+    names = [x['channel']['display_name'] for x in data['streams'] if (any(s in x['channel'].get('status', '').lower() for s in wordList) or any(x['channel'].get('display_name', '').lower() for s in streamerList)) and x['broadcast_platform']=='live']
     print(names)
 
 
     #gets the viewercounts of the people that certain words in their title
-    viewer_count = [x['viewers'] for x in data['streams'] if any (s in x['channel'].get('status', '').lower()for s in wordList or (x['channel'].get('display_name', '').lower() for s in streamerList)) and x['broadcast_platform']=='live']
+    viewer_count = [x['viewers'] for x in data['streams'] if (any(s in x['channel'].get('status', '').lower() for s in wordList) or any(x['channel'].get('display_name', '').lower() for s in streamerList)) and x['broadcast_platform']=='live']
+   
     print(viewer_count)
 
     #template = '[{0}](https://www.twitch.tv/{0}) |{10}'
