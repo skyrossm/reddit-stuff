0cc9a7b65c078a48dd2237d959eb9ddccda8df61
Ross <skyrossm@gmail.com>
2020-04-05 02:45:50 -0700

    fix bot errors

--- update_kraken.py
+++ update_kraken.py
@@ -86,18 +86,17 @@ def fetch_names():
     #print(data)
 
     #words = 'rp', 'nopixel'
-    wordList = ['nopixel', 'rp', 'roleplay', 'family', 'no pixel', 'np', 'no-pixel', 'svrp', 'twitchrp', 'aftermathrp', 'aftermath']
+    wordList = ['nopixel', ' rp ', 'roleplay', ' family ', 'no pixel', ' np ', 'no-pixel', 'svrp', 'twitchrp', 'aftermathrp', 'aftermath']
 
     #backup for streamers with stupid titles
     streamerList = ['dasMEHDI', 'koil', 's0upes', 'NewFaceSuper', 'AfriicanSnowball', 'mantisobagan', 'Madmoiselle', 'Viviana', 'JoeSmitty123', 'Xaphgnok', 'JdotField', 'the_halfhand', 'Choi', 'Armeeof1', 'NotoriousNorman', 'Jayce', 'kfruntrfrunt', 'YoinksOG', 'aXed_U', 'xReklez', 'MasterMisuri', 'Coolio']
 
-    names = [x['channel']['display_name'] for x in data['streams'] if (any(contains_word(x['channel'].get('status', '').lower(), s) for s in wordList) or any(x['channel'].get('display_name', '').lower() in streamerList)) and x['broadcast_platform']=='live']
+    names = [x['channel']['display_name'] for x in data['streams'] if (any(s in x['channel'].get('status', '').lower() for s in wordList) or any(x['channel'].get('display_name', '').lower() in streamerList)) and x['broadcast_platform']=='live']
     print(names)
 
 
     #gets the viewercounts of the people that certain words in their title
-    viewer_count = [x['viewers'] for x in data['streams'] if (any(contains_word(x['channel'].get('status', '').lower(), s) for s in wordList) or any(x['channel'].get('display_name', '').lower() in streamerList)) and x['broadcast_platform']=='live']
-    
+    viewer_count = [x['viewers'] for x in data['streams'] if (any(s in x['channel'].get('status', '').lower() for s in wordList) or any(x['channel'].get('display_name', '').lower() in streamerList)) and x['broadcast_platform']=='live']
     print(viewer_count)
     
     newlist = sorted(i for i in viewer_count if i <= 250)
